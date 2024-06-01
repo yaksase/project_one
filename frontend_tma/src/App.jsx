6dc3d200ecee1418853e1da0e80078e991076e33
yaksase <fortheplamar@gmail.com>
2024-06-01 18:16:26 +0300

    imported package for TMA

--- frontend_tma/src/App.jsx
+++ frontend_tma/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import WebApp from '@twa-dev/sdk'
 import './App.css'
 
 function App() {
@@ -21,13 +22,12 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="card">
+        <button onClick={() => WebApp.showAlert(`Current count is ${count}`)}>
+          Show Alert
+        </button>
+      </div>
     </>
   )
 }
--- frontend_tma/src/main.jsx
+++ frontend_tma/src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import WebApp from '@twa-dev/sdk'
 import './index.css'
 
+WebApp.ready();
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
