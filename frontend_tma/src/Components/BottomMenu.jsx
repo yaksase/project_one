8396d497735eb6f157d8c0f90bc2dfa59d981936
PabloTsvetkov <pcvetkov53@gmail.com>
2024-06-08 18:05:07 +0300

    bottom menu repace tailwind css with just css add all styles for bottom menu

--- frontend_tma/src/Components/BottomMenu.jsx
+++ frontend_tma/src/Components/BottomMenu.jsx
@@ -5,8 +5,8 @@ import './BottomMenu.css';
 
 export default function BottomMenu() {
   return (
-    <nav className="fixed bottom-0 transform w-full bg-black text-white">
-      <ul className="flex justify-evenly py-4 divide-x">
+    <nav class="nav_bottom_menu">
+      <ul class="icons_list_bottom_menu">
         <li>
           <a className="" href="#home">
             <IconContext.Provider value={{ size:"50"}}>
@@ -14,6 +14,7 @@ export default function BottomMenu() {
             </IconContext.Provider>
           </a>
         </li>
+        <li class="icon_divider_bottom_menu"></li>
         <li>
           <a className="" href="#inventory">
             <IconContext.Provider value={{ size:"50"}}>
@@ -21,6 +22,7 @@ export default function BottomMenu() {
             </IconContext.Provider>
           </a>
         </li>
+        <li class="icon_divider_bottom_menu"></li>
         <li>
           <a className="" href="#market">
             <IconContext.Provider value={{ size:"50"}}>
@@ -28,6 +30,7 @@ export default function BottomMenu() {
             </IconContext.Provider>
           </a>
         </li>
+        <li class="icon_divider_bottom_menu"></li>
         <li>
           <a className="" href="#crates">
             <IconContext.Provider value={{ size:"50"}}>
