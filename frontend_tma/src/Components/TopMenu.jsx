f8bcca5fc9cad69378b82b0fa89697abf6eb128c
PabloTsvetkov <pcvetkov53@gmail.com>
2024-06-08 18:35:46 +0300

    top menu styles ( not perfect )

--- frontend_tma/src/Components/TopMenu.jsx
+++ frontend_tma/src/Components/TopMenu.jsx
@@ -5,26 +5,28 @@ import './TopMenu.css';
 
 export default function BottomMenu() {
   return (
-    <nav className="fixed top-0 transform w-full bg-black text-white">
-      <ul className="flex justify-evenly">
+    // <nav className="fixed top-0 transform w-full bg-black text-white">
+    <nav class="nav_top_menu">
+      {/* <ul className="flex justify-evenly"> */}
+      <ul class="icons_list_top_menu">
         <li>
-          <a className="" href="#refferal">
-            <IconContext.Provider value={{ size:"30"}}>
-              <PiUserPlusLight></PiUserPlusLight>
-            </IconContext.Provider>
-          </a>
-        </li>
-        <li>
-          <a className="" href="#leaderboard">
-            <IconContext.Provider value={{ size:"30"}}>
-              <PiRankingLight></PiRankingLight>
-            </IconContext.Provider>
-          </a>
+          <div class="functional_icons_top_menu">
+            <a className="" href="#refferal">
+              <IconContext.Provider value={{ size: "30" }}>
+                <PiUserPlusLight class="icon"></PiUserPlusLight>
+              </IconContext.Provider>
+            </a>
+            <a className="" href="#leaderboard">
+              <IconContext.Provider value={{ size: "30" }}>
+                <PiRankingLight class="icon"></PiRankingLight>
+              </IconContext.Provider>
+            </a>
+          </div>
         </li>
         <li>
           <a className="" href="#wallet">
-            <IconContext.Provider value={{ size:"30"}}>
-              <PiWalletLight></PiWalletLight>
+            <IconContext.Provider value={{ size: "50" }}>
+              <PiWalletLight class="icon"></PiWalletLight>
             </IconContext.Provider>
           </a>
         </li>
