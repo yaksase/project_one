cd4f10cbca2060c08bef9db5e4265f0b3ba3c270
PabloTsvetkov <pcvetkov53@gmail.com>
2024-07-11 19:45:13 +0300

    remove outline in input of listAi and listPc inputs in listAi and listPc accept only numbers (но там какой-то прикол в том, что когда меняешь инпут в одной аишке меняется во всех. такого вроде как не должно быть потому что там используется usestate а он создает локальную переменную для каждого компонента, крч при создании компонентов вроде какой-то баг должен быть  :/ )

--- frontend_tma/src/Components/List/ListAi.jsx
+++ frontend_tma/src/Components/List/ListAi.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+import { useState } from 'react';
 import getAiImage from '../../utils/getAiImage';
 import GlowingButton from '../GlowingButton/GlowingButton';
 import PopUp from '../PopUp/PopUp';
@@ -6,6 +7,15 @@ import PopUp from '../PopUp/PopUp';
 import s from './List.module.css';
 
 export default function ListAi({ ai, isActive, onClose }) {
+  // function for checking validity of input
+  const [price, setPrice] = useState('');
+  const handlePriceChange = (e) => {
+    const input = e.target.value;
+    if (/^\d*$/gm.test(input)) {
+      setPrice(input);
+    }
+  };
+
   return (
     <PopUp isActive={isActive} onClose={onClose}>
       <div className={s.container}>
@@ -14,7 +24,7 @@ export default function ListAi({ ai, isActive, onClose }) {
           <span className={`text-${ai.rarity}`}>{ai.rarity}</span>
         </div>
         <div className={s.controlsContainer}>
-          <input className={s.price} placeholder='Price'></input>
+          <input className={s.price} placeholder='Price' type='text' value={price} onChange={handlePriceChange}></input>
           <div className={s.buttonContainer}>
             <GlowingButton width={'40vw'} glowSize={'0.2em'} buttonColor={'red'}>List</GlowingButton>
           </div>
--- frontend_tma/src/Components/List/ListPc.jsx
+++ frontend_tma/src/Components/List/ListPc.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+import { useState } from 'react';
 import getPcImage from '../../utils/getPcImage';
 import GlowingButton from '../GlowingButton/GlowingButton';
 import PopUp from '../PopUp/PopUp';
@@ -6,6 +7,15 @@ import PopUp from '../PopUp/PopUp';
 import s from './List.module.css';
 
 export default function ListPc({ pc, isActive, onClose }) {
+  // function for checking validity of input
+  const [price, setPrice] = useState('');
+  const handlePriceChange = (e) => {
+    const input = e.target.value;
+    if (/^\d*$/gm.test(input)) {
+      setPrice(input);
+    }
+  };
+
   return (
     <PopUp isActive={isActive} onClose={onClose}>
       <div className={s.container}>
@@ -14,7 +24,7 @@ export default function ListPc({ pc, isActive, onClose }) {
           <span className={`text-${pc.rarity}`}>{pc.rarity}</span>
         </div>
         <div className={s.controlsContainer}>
-          <input className={s.price} placeholder='Price'></input>
+          <input className={s.price} placeholder='Price' type='text' value={price} onChange={handlePriceChange}></input>
           <div className={s.buttonContainer}>
             <GlowingButton width={'40vw'} glowSize={'0.2em'} buttonColor={'red'}>List</GlowingButton>
           </div>
