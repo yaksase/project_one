c593d41cf69f79e33438070a22ded3f3f5e49984
yaksase <fortheplamar@gmail.com>
2024-07-12 14:33:34 +0300

    Added items list in market

--- frontend_tma/src/Routes/Market/Market.jsx
+++ frontend_tma/src/Routes/Market/Market.jsx
@@ -2,6 +2,8 @@
 import { useEffect, useRef, useState } from 'react';
 import { PiCaretDownBold, PiCaretUpBold } from 'react-icons/pi';
 
+import GlowingButton from '../../Components/GlowingButton/GlowingButton';
+
 import getAiImage from '../../utils/getAiImage';
 import getPcImage from '../../utils/getPcImage';
 
@@ -11,6 +13,561 @@ import s from './Market.module.css';
 import topSelectorStyle from './TopSelector.module.css';
 import itemSelectorStyle from './ItemSelector.module.css';
 import dropdownStyle from './RarityDropdown.module.css';
+import sellingItemStyle from './SellingItem.module.css';
+
+const aiItems = {
+  'common': [
+    {
+      rarity: 'common',
+      id: 0,
+      price: 12.5
+    },
+    {
+      rarity: 'common',
+      id: 1,
+      price: 12.7
+    },
+    {
+      rarity: 'common',
+      id: 2,
+      price: 13.1
+    },
+    {
+      rarity: 'common',
+      id: 3,
+      price: 14
+    },
+    {
+      rarity: 'common',
+      id: 4,
+      price: 17
+    },
+    {
+      rarity: 'common',
+      id: 5,
+      price: 20
+    },
+  ],
+  'uncommon': [
+    {
+      rarity: 'uncommon',
+      id: 0,
+      price: 70
+    },
+    {
+      rarity: 'uncommon',
+      id: 1,
+      price: 71.3
+    },
+    {
+      rarity: 'uncommon',
+      id: 2,
+      price: 75.6
+    },
+    {
+      rarity: 'uncommon',
+      id: 3,
+      price: 81.3
+    },
+    {
+      rarity: 'uncommon',
+      id: 4,
+      price: 86.9
+    },
+    {
+      rarity: 'uncommon',
+      id: 5,
+      price: 95
+    },
+    {
+      rarity: 'uncommon',
+      id: 6,
+      price: 97.5
+    },
+    {
+      rarity: 'uncommon',
+      id: 7,
+      price: 101
+    },
+    {
+      rarity: 'uncommon',
+      id: 8,
+      price: 103.4
+    },
+  ],
+  'rare': [
+    {
+      rarity: 'rare',
+      id: 0,
+      price: 194.4
+    },
+    {
+      rarity: 'rare',
+      id: 1,
+      price: 201.45
+    },
+    {
+      rarity: 'rare',
+      id: 2,
+      price: 205.67
+    },
+    {
+      rarity: 'rare',
+      id: 3,
+      price: 215
+    },
+  ],
+  'epic': [
+    {
+      rarity: 'epic',
+      id: 0,
+      price: 580.3
+    },
+    {
+      rarity: 'epic',
+      id: 1,
+      price: 594
+    },
+    {
+      rarity: 'epic',
+      id: 2,
+      price: 601
+    },
+    {
+      rarity: 'epic',
+      id: 3,
+      price: 602.3
+    },
+    {
+      rarity: 'epic',
+      id: 4,
+      price: 605
+    },
+    {
+      rarity: 'epic',
+      id: 5,
+      price: 630
+    },
+  ],
+  'legendary': [
+    {
+      rarity: 'legendary',
+      id: 0,
+      price: 2045
+    },
+    {
+      rarity: 'legendary',
+      id: 1,
+      price: 2076.9
+    },
+    {
+      rarity: 'legendary',
+      id: 2,
+      price: 2080
+    },
+    {
+      rarity: 'legendary',
+      id: 3,
+      price: 2094
+    },
+    {
+      rarity: 'legendary',
+      id: 4,
+      price: 2108.3
+    },
+    {
+      rarity: 'legendary',
+      id: 5,
+      price: 2110
+    },
+    {
+      rarity: 'legendary',
+      id: 6,
+      price: 2115.54
+    },
+    {
+      rarity: 'legendary',
+      id: 7,
+      price: 2116
+    },
+    {
+      rarity: 'legendary',
+      id: 8,
+      price: 2160
+    },
+    {
+      rarity: 'legendary',
+      id: 9,
+      price: 2190.3
+    },
+    {
+      rarity: 'legendary',
+      id: 10,
+      price: 2201
+    },
+    {
+      rarity: 'legendary',
+      id: 11,
+      price: 2203.3
+    },
+    {
+      rarity: 'legendary',
+      id: 12,
+      price: 2210
+    },
+    {
+      rarity: 'legendary',
+      id: 13,
+      price: 2216.2
+    },
+    {
+      rarity: 'legendary',
+      id: 14,
+      price: 2356
+    },
+    {
+      rarity: 'legendary',
+      id: 15,
+      price: 2708.9
+    },
+    {
+      rarity: 'legendary',
+      id: 16,
+      price: 3105.3
+    },
+    {
+      rarity: 'legendary',
+      id: 17,
+      price: 3150
+    },
+  ],
+  'ultra': [
+    {
+      rarity: 'ultra',
+      id: 0,
+      price: 8095
+    },
+    {
+      rarity: 'ultra',
+      id: 1,
+      price: 8345.3
+    },
+    {
+      rarity: 'ultra',
+      id: 2,
+      price: 8347
+    },
+    {
+      rarity: 'ultra',
+      id: 3,
+      price: 8450
+    },
+    {
+      rarity: 'ultra',
+      id: 4,
+      price: 8556
+    },
+    {
+      rarity: 'ultra',
+      id: 5,
+      price: 10230.32
+    },
+  ],
+  'mythic': [
+    {
+      rarity: 'mythic',
+      id: 0,
+      price: 56980
+    },
+    {
+      rarity: 'mythic',
+      id: 1,
+      price: 59000
+    },
+    {
+      rarity: 'mythic',
+      id: 2,
+      price: 62000
+    },
+  ],
+};
+
+const pcItems = {
+  'common': [
+    {
+      rarity: 'common',
+      id: 0,
+      price: 12.5
+    },
+    {
+      rarity: 'common',
+      id: 1,
+      price: 12.7
+    },
+    {
+      rarity: 'common',
+      id: 2,
+      price: 13.1
+    },
+    {
+      rarity: 'common',
+      id: 3,
+      price: 14
+    },
+    {
+      rarity: 'common',
+      id: 4,
+      price: 17
+    },
+    {
+      rarity: 'common',
+      id: 5,
+      price: 20
+    },
+  ],
+  'uncommon': [
+    {
+      rarity: 'uncommon',
+      id: 0,
+      price: 70
+    },
+    {
+      rarity: 'uncommon',
+      id: 1,
+      price: 71.3
+    },
+    {
+      rarity: 'uncommon',
+      id: 2,
+      price: 75.6
+    },
+    {
+      rarity: 'uncommon',
+      id: 3,
+      price: 81.3
+    },
+    {
+      rarity: 'uncommon',
+      id: 4,
+      price: 86.9
+    },
+    {
+      rarity: 'uncommon',
+      id: 5,
+      price: 95
+    },
+    {
+      rarity: 'uncommon',
+      id: 6,
+      price: 97.5
+    },
+    {
+      rarity: 'uncommon',
+      id: 7,
+      price: 101
+    },
+    {
+      rarity: 'uncommon',
+      id: 8,
+      price: 103.4
+    },
+  ],
+  'rare': [
+    {
+      rarity: 'rare',
+      id: 0,
+      price: 194.4
+    },
+    {
+      rarity: 'rare',
+      id: 1,
+      price: 201.45
+    },
+    {
+      rarity: 'rare',
+      id: 2,
+      price: 205.67
+    },
+    {
+      rarity: 'rare',
+      id: 3,
+      price: 215
+    },
+  ],
+  'epic': [
+    {
+      rarity: 'epic',
+      id: 0,
+      price: 580.3
+    },
+    {
+      rarity: 'epic',
+      id: 1,
+      price: 594
+    },
+    {
+      rarity: 'epic',
+      id: 2,
+      price: 601
+    },
+    {
+      rarity: 'epic',
+      id: 3,
+      price: 602.3
+    },
+    {
+      rarity: 'epic',
+      id: 4,
+      price: 605
+    },
+    {
+      rarity: 'epic',
+      id: 5,
+      price: 630
+    },
+  ],
+  'legendary': [
+    {
+      rarity: 'legendary',
+      id: 0,
+      price: 2045
+    },
+    {
+      rarity: 'legendary',
+      id: 1,
+      price: 2076.9
+    },
+    {
+      rarity: 'legendary',
+      id: 2,
+      price: 2080
+    },
+    {
+      rarity: 'legendary',
+      id: 3,
+      price: 2094
+    },
+    {
+      rarity: 'legendary',
+      id: 4,
+      price: 2108.3
+    },
+    {
+      rarity: 'legendary',
+      id: 5,
+      price: 2110
+    },
+    {
+      rarity: 'legendary',
+      id: 6,
+      price: 2115.54
+    },
+    {
+      rarity: 'legendary',
+      id: 7,
+      price: 2116
+    },
+    {
+      rarity: 'legendary',
+      id: 8,
+      price: 2160
+    },
+    {
+      rarity: 'legendary',
+      id: 9,
+      price: 2190.3
+    },
+    {
+      rarity: 'legendary',
+      id: 10,
+      price: 2201
+    },
+    {
+      rarity: 'legendary',
+      id: 11,
+      price: 2203.3
+    },
+    {
+      rarity: 'legendary',
+      id: 12,
+      price: 2210
+    },
+    {
+      rarity: 'legendary',
+      id: 13,
+      price: 2216.2
+    },
+    {
+      rarity: 'legendary',
+      id: 14,
+      price: 2356
+    },
+    {
+      rarity: 'legendary',
+      id: 15,
+      price: 2708.9
+    },
+    {
+      rarity: 'legendary',
+      id: 16,
+      price: 3105.3
+    },
+    {
+      rarity: 'legendary',
+      id: 17,
+      price: 3150
+    },
+  ],
+  'ultra': [
+    {
+      rarity: 'ultra',
+      id: 0,
+      price: 8095
+    },
+    {
+      rarity: 'ultra',
+      id: 1,
+      price: 8345.3
+    },
+    {
+      rarity: 'ultra',
+      id: 2,
+      price: 8347
+    },
+    {
+      rarity: 'ultra',
+      id: 3,
+      price: 8450
+    },
+    {
+      rarity: 'ultra',
+      id: 4,
+      price: 8556
+    },
+    {
+      rarity: 'ultra',
+      id: 5,
+      price: 10230.32
+    },
+  ],
+  'mythic': [
+    {
+      rarity: 'mythic',
+      id: 0,
+      price: 56980
+    },
+    {
+      rarity: 'mythic',
+      id: 1,
+      price: 59000
+    },
+    {
+      rarity: 'mythic',
+      id: 2,
+      price: 62000
+    },
+  ],
+};
 
 const raritiesData = [
   'common', 'uncommon', 'rare', 'epic', 'legendary', 'ultra', 'mythic'
@@ -52,6 +609,19 @@ function RarityDropdown({ imgSrc, children, showDropdown, setShowDropdown }) {
   )
 }
 
+function SellingItem({ onBuyClick, imgSrc, price }) {
+  return (
+    <div className={sellingItemStyle.container}>
+      <img src={imgSrc}></img>
+      <span className='priceWrapper'>
+        {price}
+        <img src={tokenImage}></img>
+      </span>
+      <GlowingButton glowSize={'0.3rem'} width={'100%'} verticalPadding='0.2em'>Buy</GlowingButton>
+    </div>
+  )
+}
+
 export default function Market() {
   const [showMarket, setShowMarket] = useState(true);
 
@@ -67,71 +637,94 @@ export default function Market() {
   const [showPcDropdown, setShowPcDropdown] = useState(false);
 
   return (
-    <div className={s.container}>
-      <div className={s.topSelector}>
-        <button className={`${topSelectorStyle.button} ${showMarket ? topSelectorStyle.active : ''}`} onClick={() => {
-          setShowMarket(true);
-        }}>Market</button>
-        <button className={`${topSelectorStyle.button} ${showMarket ? '' : topSelectorStyle.active}`} onClick={() => {
-          setShowMarket(false);
-        }}>Listed</button>
+    <>
+      <div className={s.container}>
+        <div className={s.topSelector}>
+          <button className={`${topSelectorStyle.button} ${showMarket ? topSelectorStyle.active : ''}`} onClick={() => {
+            setShowMarket(true);
+          }}>Market</button>
+          <button className={`${topSelectorStyle.button} ${showMarket ? '' : topSelectorStyle.active}`} onClick={() => {
+            setShowMarket(false);
+          }}>Listed</button>
+        </div>
+        <div className={s.itemSelector}>
+          {
+            showAi ?
+              <RarityDropdown imgSrc={getAiImage(curAi)} showDropdown={showAiDropdown} setShowDropdown={setShowAiDropdown}>
+                {raritiesData.map((rarity) => {
+                  return (
+                    <div key={rarity} className={s.dropdownItem} onClick={() => {
+                      setCurAi(rarity);
+                      setShowAiDropdown(false);
+                    }}>
+                      <img src={getAiImage(rarity)} style={{ width: '1rem' }}></img>
+                      <span className={`text-${rarity}`}>{rarity}</span>
+                    </div>
+                  )
+                })}
+              </RarityDropdown> :
+              <button className={itemSelectorStyle.button} onClick={() => {
+                setShowAi(true);
+                setShowPc(false);
+                setShowToken(false);
+              }}>
+                <img src={getAiImage(curAi)}></img>
+              </button>
+          }
+          {
+            showPc ?
+              <RarityDropdown imgSrc={getPcImage(curPc)} showDropdown={showPcDropdown} setShowDropdown={setShowPcDropdown}>
+                {raritiesData.map((rarity) => {
+                  return (
+                    <div key={rarity} className={s.dropdownItem} onClick={() => {
+                      setCurPc(rarity);
+                      setShowPcDropdown(false);
+                    }}>
+                      <img src={getPcImage(rarity)} style={{ width: '1.5rem' }}></img>
+                      <span className={`text-${rarity}`}>{rarity}</span>
+                    </div>
+                  )
+                })}
+              </RarityDropdown> :
+              <button className={itemSelectorStyle.button} onClick={() => {
+                setShowPc(true);
+                setShowAi(false);
+                setShowToken(false);
+              }}>
+                <img src={getPcImage(curPc)}></img>
+                {showPc ? <PiCaretDownBold></PiCaretDownBold> : <></>}
+              </button>
+          }
+          <button className={`${itemSelectorStyle.button} ${showToken ? itemSelectorStyle.active : ''}`} onClick={() => {
+            setShowToken(true);
+            setShowAi(false);
+            setShowPc(false);
+          }}>
+            <img src={tokenImage}></img>
+          </button>
+        </div>
+        <div className={s.itemsContainer}>
+          {
+            showAi ?
+            aiItems[curAi].map((ai) => {
+              return (
+                <SellingItem key={ai.id} imgSrc={getAiImage(ai.rarity)} price={ai.price}></SellingItem>
+              )
+            }) :
+            <></>
+          }
+          {
+            showPc ?
+            pcItems[curPc].map((pc) => {
+              return (
+                <SellingItem key={pc.id} imgSrc={getPcImage(pc.rarity)} price={pc.price}></SellingItem>
+              )
+            }) :
+            <></>
+          }
+        </div>
       </div>
-      <div className={s.itemSelector}>
-        {
-          showAi ?
-            <RarityDropdown imgSrc={getAiImage(curAi)} showDropdown={showAiDropdown} setShowDropdown={setShowAiDropdown}>
-              {raritiesData.map((rarity) => {
-                return (
-                  <div key={rarity} className={s.dropdownItem} onClick={() => {
-                    setCurAi(rarity);
-                    setShowAiDropdown(false);
-                  }}>
-                    <img src={getAiImage(rarity)} style={{ width: '1rem' }}></img>
-                    <span className={`text-${rarity}`}>{rarity}</span>
-                  </div>
-                )
-              })}
-            </RarityDropdown> :
-            <button className={itemSelectorStyle.button} onClick={() => {
-              setShowAi(true);
-              setShowPc(false);
-              setShowToken(false);
-            }}>
-              <img src={getAiImage(curAi)}></img>
-            </button>
-        }
-        {
-          showPc ?
-            <RarityDropdown imgSrc={getPcImage(curPc)} showDropdown={showPcDropdown} setShowDropdown={setShowPcDropdown}>
-              {raritiesData.map((rarity) => {
-                return (
-                  <div key={rarity} className={s.dropdownItem} onClick={() => {
-                    setCurPc(rarity);
-                    setShowPcDropdown(false);
-                  }}>
-                    <img src={getPcImage(rarity)} style={{ width: '1.5rem' }}></img>
-                    <span className={`text-${rarity}`}>{rarity}</span>
-                  </div>
-                )
-              })}
-            </RarityDropdown> :
-            <button className={itemSelectorStyle.button} onClick={() => {
-              setShowPc(true);
-              setShowAi(false);
-              setShowToken(false);
-            }}>
-              <img src={getPcImage(curPc)}></img>
-              {showPc ? <PiCaretDownBold></PiCaretDownBold> : <></>}
-            </button>
-        }
-        <button className={`${itemSelectorStyle.button} ${showToken ? itemSelectorStyle.active : ''}`} onClick={() => {
-          setShowToken(true);
-          setShowAi(false);
-          setShowPc(false);
-        }}>
-          <img src={tokenImage}></img>
-        </button>
-      </div>
-    </div>
+    </>
+    
   )
 }
