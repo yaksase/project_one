adda2455687d93aeac8df5e2c5fc026791b60463
PabloTsvetkov <pcvetkov53@gmail.com>
2024-07-21 11:10:47 +0300

    refferals popup base version ( lately will rebuild fonts sizes, letter spacing and other stuff )

--- frontend_tma/src/Routes/Invite/Invite.jsx
+++ frontend_tma/src/Routes/Invite/Invite.jsx
@@ -1,12 +1,32 @@
 /* eslint-disable react/prop-types */
+import { FaLink } from "react-icons/fa";
+import { GoPeople } from "react-icons/go";
+import { PiCoinsLight, PiHouseLight } from "react-icons/pi";
+
 import PopUp from '../../Components/PopUp/PopUp';
+import GlowingButton from "../../Components/GlowingButton/GlowingButton";
 
 import s from './Invite.module.css';
 
 export default function Invite({ isActive, onClose }) {
   return (
     <PopUp isActive={isActive} onClose={onClose}>
-      
+      <div className={s.invitationContent}>
+        <span className={s.invitationTitle}>Refferals</span>
+        <span className={s.hint}>Get 10% of each friend you bring</span>
+        <GlowingButton><div className={s.buttonContent}>Invite friend<div className={`priceWrapper`}><FaLink /></div></div></GlowingButton>
+        <div className={s.inviteStats}>
+          <div className={s.totalRefferalsBlock}>
+            <span>Total refferals:</span>
+            <span><GoPeople />66</span>
+          </div>
+          <div className={s.totalEarningBlock}>
+            <span>Total earning:</span>
+            <span><PiCoinsLight className={`priceWrapper`}/>12000$Po</span>
+          </div>
+        </div>
+        <PiHouseLight className={s.homeIcon}/>
+      </div>
     </PopUp>
   )
 }
