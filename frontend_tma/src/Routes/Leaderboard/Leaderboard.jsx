48d2fe77ecda1ffae69f84b494a50e8f885c30a1
PabloTsvetkov <pcvetkov53@gmail.com>
2024-07-24 13:39:26 +0300

    1). remove sorting in the leaderboard + add margin in the bottom 2). remove empty space in invite popUp 3). poor version of wallet page (  )

--- frontend_tma/src/Routes/Leaderboard/Leaderboard.jsx
+++ frontend_tma/src/Routes/Leaderboard/Leaderboard.jsx
@@ -31,27 +31,207 @@ export default function Leaderboard({ isActive, onClose }) {
       id: 5,
       name: 'jesus',
       tokens: 0
-    }
+    },
+    {
+      id: 1,
+      name: 'bob',
+      tokens: 1234
+    },
+    {
+      id: 2,
+      name: 'lol',
+      tokens: 123
+    },
+    {
+      id: 3,
+      name: 'francua modi dick lil uzi vert diamond',
+      tokens: 98765432445
+    },
+    {
+      id: 4,
+      name: 'lil morty',
+      tokens: 432
+    },
+    {
+      id: 1,
+      name: 'bob',
+      tokens: 1234
+    },
+    {
+      id: 2,
+      name: 'lol',
+      tokens: 123
+    },
+    {
+      id: 3,
+      name: 'francua modi dick lil uzi vert diamond',
+      tokens: 98765432445
+    },
+    {
+      id: 4,
+      name: 'lil morty',
+      tokens: 432
+    },
+    {
+      id: 1,
+      name: 'bob',
+      tokens: 1234
+    },
+    {
+      id: 2,
+      name: 'lol',
+      tokens: 123
+    },
+    {
+      id: 3,
+      name: 'francua modi dick lil uzi vert diamond',
+      tokens: 98765432445
+    },
+    {
+      id: 4,
+      name: 'lil morty',
+      tokens: 432
+    },
+    {
+      id: 1,
+      name: 'bob',
+      tokens: 1234
+    },
+    {
+      id: 2,
+      name: 'lol',
+      tokens: 123
+    },
+    {
+      id: 3,
+      name: 'francua modi dick lil uzi vert diamond',
+      tokens: 98765432445
+    },
+    {
+      id: 4,
+      name: 'lil morty',
+      tokens: 432
+    },
+    {
+      id: 1,
+      name: 'bob',
+      tokens: 1234
+    },
+    {
+      id: 2,
+      name: 'lol',
+      tokens: 123
+    },
+    {
+      id: 3,
+      name: 'francua modi dick lil uzi vert diamond',
+      tokens: 98765432445
+    },
+    {
+      id: 4,
+      name: 'lil morty',
+      tokens: 432
+    },
+    {
+      id: 1,
+      name: 'bob',
+      tokens: 1234
+    },
+    {
+      id: 2,
+      name: 'lol',
+      tokens: 123
+    },
+    {
+      id: 3,
+      name: 'francua modi dick lil uzi vert diamond',
+      tokens: 98765432445
+    },
+    {
+      id: 4,
+      name: 'lil morty',
+      tokens: 432
+    },
+    {
+      id: 1,
+      name: 'bob',
+      tokens: 1234
+    },
+    {
+      id: 2,
+      name: 'lol',
+      tokens: 123
+    },
+    {
+      id: 3,
+      name: 'francua modi dick lil uzi vert diamond',
+      tokens: 98765432445
+    },
+    {
+      id: 4,
+      name: 'lil morty',
+      tokens: 432
+    },
+    {
+      id: 1,
+      name: 'bob',
+      tokens: 1234
+    },
+    {
+      id: 2,
+      name: 'lol',
+      tokens: 123
+    },
+    {
+      id: 3,
+      name: 'francua modi dick lil uzi vert diamond',
+      tokens: 98765432445
+    },
+    {
+      id: 4,
+      name: 'lil morty',
+      tokens: 432
+    },
+    {
+      id: 1,
+      name: 'bob',
+      tokens: 1234
+    },
+    {
+      id: 2,
+      name: 'lol',
+      tokens: 123
+    },
+    {
+      id: 3,
+      name: 'francua modi dick lil uzi vert diamond',
+      tokens: 98765432445
+    },
+    {
+      id: 4,
+      name: 'lil morty',
+      tokens: 432
+    },
   ];
 
   return (
     <PopUp isActive={isActive} onClose={onClose}>
       <div className={s.leaderboardPopUp}>
         <span className={s.leaderboardTitle}>Leaderboard</span>
-
-        {
-          userList.sort((a, b) => b.tokens - a.tokens).map((user, index) => (
-            <div className={s.leaderboardItem}>
-              <div className={s.itemPlace}>
-                {index + 1}
+        <div className={s.usersList}>
+          {
+            userList.map((user, index) => (
+              <div key={index} className={s.leaderboardItem}>
+                <div className={s.itemPlace}>
+                  {index + 1}
+                </div>
+                <div className={s.itemDescription}>
+                  {user.name} {user.tokens} <span className={`priceWrapper`}><img src={tokenIcon}></img></span>
+                </div>
               </div>
-              <div className={s.itemDescription}>
-                {user.name} {user.tokens} <span className={`priceWrapper`}><img src={tokenIcon}></img></span>
-              </div>
-            </div>
-          ))
-        }
-
+            ))
+          }
+        </div>
       </div>
     </PopUp>
   )
--- frontend_tma/src/Routes/Wallet/Wallet.jsx
+++ frontend_tma/src/Routes/Wallet/Wallet.jsx
@@ -1,12 +1,68 @@
 /* eslint-disable react/prop-types */
+import { useState } from 'react';
 import PopUp from '../../Components/PopUp/PopUp';
 
 import s from './Wallet.module.css';
 
-export default function Leaderboard({ isActive, onClose }) {
+import { FaLink } from "react-icons/fa";
+
+export default function Wallet({ isActive, onClose }) {
+  const [showDepositPopup, setShowDepositPopup] = useState(false);
+  const [showWithdrawPopup, setShowWithdrawPopup] = useState(false);
+  const [showWalletPopUp, setShowWalletPopUp] = useState(isActive);
+
+  const openDepositPopup = () => {
+    setShowDepositPopup(true);
+  };
+  const openWithdrawPopup = () => {
+    setShowWithdrawPopup(true);
+  };
+  const closeDepositPopup = () => {
+    setShowDepositPopup(false);
+    setShowWalletPopUp(false);
+  };
+  const closeWithdrawPopup = () => {
+    setShowWithdrawPopup(false);
+    setShowWalletPopUp(false);
+  };
+  const closeAll = () => {
+    setShowDepositPopup(false);
+    setShowWalletPopUp(false);
+    setShowWithdrawPopup(false);
+  };
   return (
-    <PopUp isActive={isActive} onClose={onClose}>
-      
-    </PopUp>
+    <>
+      <PopUp isActive={isActive} onClose={onClose}>
+        <div className={s.blockContent}>
+          <div className={s.inputBlock}>
+            <input type="text"></input>
+          </div>
+          <div className={s.buttonsBlock}>
+            <button className={s.buttonDeposit} onClick={openDepositPopup}>Deposit</button>
+            <button className={s.buttonWithdraw} onClick={openWithdrawPopup}>Withdraw</button>
+          </div>
+        </div>
+      </PopUp>
+
+      <PopUp isActive={showDepositPopup} onClose={closeDepositPopup}>
+        <div className={s.depositContent}>
+          <span className={`${s.firstGreenHint} ${s.centerText} greenHighlight`}>To top up your balance, transfer the required amount of $Ton to the specified wallet</span>
+          <div className={s.inputBlockDeposit}>
+            <label>Network: Ton</label>
+            <div className={s.inputString}>
+              <input type="text"></input>
+              <FaLink className={s.linkIcon}></FaLink>
+            </div>
+          </div>
+          <span className={`${s.secondHint} ${s.centerText}`}>Your balance will be replenished automatically for the amount that you transferred immediately after the transaction takes place.</span>
+          <span className={`${s.attention} ${s.centerText} text-ultra`}>Attention!</span>
+          <span className={`${s.attentionDescription} ${s.centerText} text-ultra`}>Send cryptocurrency only from the wallet linked to the application, otherwise your funds will be lost.</span>
+        </div>
+      </PopUp>
+
+      <PopUp isActive={showWithdrawPopup} onClose={closeWithdrawPopup}>
+
+      </PopUp>
+    </>
   )
 }
