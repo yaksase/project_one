f7f883da1012695f651da9f87d56651a58991f61
yaksase <fortheplamar@gmail.com>
2024-08-01 23:44:08 +0300

    connected leaderboard endpoint

--- frontend_tma/src/Routes/Leaderboard/Leaderboard.jsx
+++ frontend_tma/src/Routes/Leaderboard/Leaderboard.jsx
@@ -1,10 +1,221 @@
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import PopUp from '../../Components/PopUp/PopUp';
 
 import tokenIcon from '../../assets/token_icon.png';
 
+import axiosInstance from '../../axios';
+
 import s from './Leaderboard.module.css';
+
+const userList = [
+  {
+    id: 1,
+    name: 'bob',
+    tokens: 1234
+  },
+  {
+    id: 2,
+    name: 'lol',
+    tokens: 123
+  },
+  {
+    id: 3,
+    name: 'francua modi dick lil uzi vert diamond',
+    tokens: 98765432445
+  },
+  {
+    id: 4,
+    name: 'lil morty',
+    tokens: 432
+  },
+  {
+    id: 5,
+    name: 'jesus',
+    tokens: 0
+  },
+  {
+    id: 1,
+    name: 'bob',
+    tokens: 1234
+  },
+  {
+    id: 2,
+    name: 'lol',
+    tokens: 123
+  },
+  {
+    id: 3,
+    name: 'francua modi dick lil uzi vert diamond',
+    tokens: 98765432445
+  },
+  {
+    id: 4,
+    name: 'lil morty',
+    tokens: 432
+  },
+  {
+    id: 1,
+    name: 'bob',
+    tokens: 1234
+  },
+  {
+    id: 2,
+    name: 'lol',
+    tokens: 123
+  },
+  {
+    id: 3,
+    name: 'francua modi dick lil uzi vert diamond',
+    tokens: 98765432445
+  },
+  {
+    id: 4,
+    name: 'lil morty',
+    tokens: 432
+  },
+  {
+    id: 1,
+    name: 'bob',
+    tokens: 1234
+  },
+  {
+    id: 2,
+    name: 'lol',
+    tokens: 123
+  },
+  {
+    id: 3,
+    name: 'francua modi dick lil uzi vert diamond',
+    tokens: 98765432445
+  },
+  {
+    id: 4,
+    name: 'lil morty',
+    tokens: 432
+  },
+  {
+    id: 1,
+    name: 'bob',
+    tokens: 1234
+  },
+  {
+    id: 2,
+    name: 'lol',
+    tokens: 123
+  },
+  {
+    id: 3,
+    name: 'francua modi dick lil uzi vert diamond',
+    tokens: 98765432445
+  },
+  {
+    id: 4,
+    name: 'lil morty',
+    tokens: 432
+  },
+  {
+    id: 1,
+    name: 'bob',
+    tokens: 1234
+  },
+  {
+    id: 2,
+    name: 'lol',
+    tokens: 123
+  },
+  {
+    id: 3,
+    name: 'francua modi dick lil uzi vert diamond',
+    tokens: 98765432445
+  },
+  {
+    id: 4,
+    name: 'lil morty',
+    tokens: 432
+  },
+  {
+    id: 1,
+    name: 'bob',
+    tokens: 1234
+  },
+  {
+    id: 2,
+    name: 'lol',
+    tokens: 123
+  },
+  {
+    id: 3,
+    name: 'francua modi dick lil uzi vert diamond',
+    tokens: 98765432445
+  },
+  {
+    id: 4,
+    name: 'lil morty',
+    tokens: 432
+  },
+  {
+    id: 1,
+    name: 'bob',
+    tokens: 1234
+  },
+  {
+    id: 2,
+    name: 'lol',
+    tokens: 123
+  },
+  {
+    id: 3,
+    name: 'francua modi dick lil uzi vert diamond',
+    tokens: 98765432445
+  },
+  {
+    id: 4,
+    name: 'lil morty',
+    tokens: 432
+  },
+  {
+    id: 1,
+    name: 'bob',
+    tokens: 1234
+  },
+  {
+    id: 2,
+    name: 'lol',
+    tokens: 123
+  },
+  {
+    id: 3,
+    name: 'francua modi dick lil uzi vert diamond',
+    tokens: 98765432445
+  },
+  {
+    id: 4,
+    name: 'lil morty',
+    tokens: 432
+  },
+  {
+    id: 1,
+    name: 'bob',
+    tokens: 1234
+  },
+  {
+    id: 2,
+    name: 'lol',
+    tokens: 123
+  },
+  {
+    id: 3,
+    name: 'francua modi dick lil uzi vert diamond',
+    tokens: 98765432445
+  },
+  {
+    id: 4,
+    name: 'lil morty',
+    tokens: 432
+  },
+];
 
 Leaderboard.propTypes = {
   isActive: PropTypes.bool.isRequired,
@@ -12,213 +223,15 @@ Leaderboard.propTypes = {
 }
 
 export default function Leaderboard({ isActive, onClose }) {
-  const userList = [
-    {
-      id: 1,
-      name: 'bob',
-      tokens: 1234
-    },
-    {
-      id: 2,
-      name: 'lol',
-      tokens: 123
-    },
-    {
-      id: 3,
-      name: 'francua modi dick lil uzi vert diamond',
-      tokens: 98765432445
-    },
-    {
-      id: 4,
-      name: 'lil morty',
-      tokens: 432
-    },
-    {
-      id: 5,
-      name: 'jesus',
-      tokens: 0
-    },
-    {
-      id: 1,
-      name: 'bob',
-      tokens: 1234
-    },
-    {
-      id: 2,
-      name: 'lol',
-      tokens: 123
-    },
-    {
-      id: 3,
-      name: 'francua modi dick lil uzi vert diamond',
-      tokens: 98765432445
-    },
-    {
-      id: 4,
-      name: 'lil morty',
-      tokens: 432
-    },
-    {
-      id: 1,
-      name: 'bob',
-      tokens: 1234
-    },
-    {
-      id: 2,
-      name: 'lol',
-      tokens: 123
-    },
-    {
-      id: 3,
-      name: 'francua modi dick lil uzi vert diamond',
-      tokens: 98765432445
-    },
-    {
-      id: 4,
-      name: 'lil morty',
-      tokens: 432
-    },
-    {
-      id: 1,
-      name: 'bob',
-      tokens: 1234
-    },
-    {
-      id: 2,
-      name: 'lol',
-      tokens: 123
-    },
-    {
-      id: 3,
-      name: 'francua modi dick lil uzi vert diamond',
-      tokens: 98765432445
-    },
-    {
-      id: 4,
-      name: 'lil morty',
-      tokens: 432
-    },
-    {
-      id: 1,
-      name: 'bob',
-      tokens: 1234
-    },
-    {
-      id: 2,
-      name: 'lol',
-      tokens: 123
-    },
-    {
-      id: 3,
-      name: 'francua modi dick lil uzi vert diamond',
-      tokens: 98765432445
-    },
-    {
-      id: 4,
-      name: 'lil morty',
-      tokens: 432
-    },
-    {
-      id: 1,
-      name: 'bob',
-      tokens: 1234
-    },
-    {
-      id: 2,
-      name: 'lol',
-      tokens: 123
-    },
-    {
-      id: 3,
-      name: 'francua modi dick lil uzi vert diamond',
-      tokens: 98765432445
-    },
-    {
-      id: 4,
-      name: 'lil morty',
-      tokens: 432
-    },
-    {
-      id: 1,
-      name: 'bob',
-      tokens: 1234
-    },
-    {
-      id: 2,
-      name: 'lol',
-      tokens: 123
-    },
-    {
-      id: 3,
-      name: 'francua modi dick lil uzi vert diamond',
-      tokens: 98765432445
-    },
-    {
-      id: 4,
-      name: 'lil morty',
-      tokens: 432
-    },
-    {
-      id: 1,
-      name: 'bob',
-      tokens: 1234
-    },
-    {
-      id: 2,
-      name: 'lol',
-      tokens: 123
-    },
-    {
-      id: 3,
-      name: 'francua modi dick lil uzi vert diamond',
-      tokens: 98765432445
-    },
-    {
-      id: 4,
-      name: 'lil morty',
-      tokens: 432
-    },
-    {
-      id: 1,
-      name: 'bob',
-      tokens: 1234
-    },
-    {
-      id: 2,
-      name: 'lol',
-      tokens: 123
-    },
-    {
-      id: 3,
-      name: 'francua modi dick lil uzi vert diamond',
-      tokens: 98765432445
-    },
-    {
-      id: 4,
-      name: 'lil morty',
-      tokens: 432
-    },
-    {
-      id: 1,
-      name: 'bob',
-      tokens: 1234
-    },
-    {
-      id: 2,
-      name: 'lol',
-      tokens: 123
-    },
-    {
-      id: 3,
-      name: 'francua modi dick lil uzi vert diamond',
-      tokens: 98765432445
-    },
-    {
-      id: 4,
-      name: 'lil morty',
-      tokens: 432
-    },
-  ];
+  const [users, setUsers] = useState(userList);
+  
+  useEffect(() => {
+    axiosInstance.get('/api/leaderboard')
+    .then((res) => {
+      setUsers(res.data);
+    })
+    .catch((err) => console.log(err))
+  }, []);
 
   return (
     <PopUp isActive={isActive} onClose={onClose}>
@@ -226,7 +239,7 @@ export default function Leaderboard({ isActive, onClose }) {
         <span className={s.leaderboardTitle}>Leaderboard</span>
         <div className={s.usersList}>
           {
-            userList.map((user, index) => (
+            users.map((user, index) => (
               <div key={index} className={s.leaderboardItem}>
                 <div className={s.itemPlace}>
                   {index + 1}
