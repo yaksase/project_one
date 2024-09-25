9cf264c74745283acfd5ae7b5bcda29121156cf5
yaksase <fortheplamar@gmail.com>
2024-09-26 01:16:48 +0300

    partailly connected Inventory to the API

--- frontend_tma/src/Routes/Inventory/Inventory.jsx
+++ frontend_tma/src/Routes/Inventory/Inventory.jsx
@@ -1,13 +1,17 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { PiCoinVerticalLight, PiInfoLight, PiPercentLight, PiDesktopLight, PiArrowsClockwiseLight, PiClockLight, PiClockBold, PiDesktopBold, PiCheckBold } from 'react-icons/pi';
+import { PiCoinVerticalLight, PiInfoLight, PiPercentLight, PiDesktopLight, PiArrowsClockwiseLight, PiClockLight, PiCheckBold } from 'react-icons/pi';
 
 import GlowingButton from '../../Components/GlowingButton/GlowingButton';
 import PopUp from '../../Components/PopUp/PopUp';
 import ConnectPc from '../../Components/ConnectPc/ConnectPc';
 import PositiveNotification from '../../Components/PositiveNotification/PositiveNotification';
 import Input from '../../Components/Input/Input';
+import LoadingScreen from '../../Components/LoadingScreen/LoadingScreen.jsx';
+import AiGeneralInfo from '../../Components/ItemGeneralInfo/AiGeneralInfo.jsx';
+import PcGeneralInfo from '../../Components/ItemGeneralInfo/PcGeneralInfo.jsx';
 
+import getRarity from '../../utils/getRarity.js';
 import getAiImage from '../../utils/getAiImage.js';
 import getPcImage from '../../utils/getPcImage.js';
 
@@ -18,125 +22,7 @@ import actSelStyle from './ActivatedSelector.module.css';
 import typeSelStyle from './TypeSelector.module.css';
 import infoPopUpStyle from './InfoPopUp.module.css';
 import listStyle from './List.module.css';
-
-// 1 is activated 0 is not activated
-const aiData = [
-  {
-    id: 9922,
-    rarity: 'common',
-    state: 1,
-    timer: 2,
-    maxSlots: 8,
-    slots: 5,
-  },
-  {
-    id: 12,
-    rarity: 'uncommon',
-    state: 0,
-    timer: 4,
-    maxSlots: 10,
-    slots: 5,
-  },
-  {
-    id: 444,
-    rarity: 'ultra',
-    state: 1,
-    timer: 8,
-    maxSlots: 20,
-    slots: 18,
-  },
-  {
-    id: 99221,
-    rarity: 'epic',
-    state: 1,
-    timer: 10,
-    maxSlots: 15,
-    slots: 12,
-  },
-  {
-    id: 121,
-    rarity: 'rare',
-    state: 0,
-    timer: 6,
-    maxSlots: 12,
-    slots: 10,
-  },
-  {
-    id: 4414,
-    rarity: 'legendary',
-    state: 1,
-    timer: 12,
-    maxSlots: 18,
-    slots: 18,
-  },
-  {
-    id: 444112312321,
-    rarity: 'mythic',
-    state: 1,
-    timer: 20,
-    maxSlots: 40,
-    slots: 35,
-  },
-];
-
-const pcData = [
-  {
-    id: 123,
-    rarity: 'common',
-    state: 1,
-    tokenCount: 1,
-    slotsCount: 1,
-    health: 50,
-  },
-  {
-    id: 2,
-    rarity: 'ultra',
-    state: 1,
-    tokenCount: 50,
-    slotsCount: 6,
-    health: 75,
-  },
-  {
-    id: 9,
-    rarity: 'mythic',
-    state: 0,
-    tokenCount: 100,
-    slotsCount: 7,
-    health: 12,
-  },
-  {
-    id: 1231,
-    rarity: 'uncommon',
-    state: 1,
-    tokenCount: 2,
-    slotsCount: 2,
-    health: 89,
-  },
-  {
-    id: 21,
-    rarity: 'rare',
-    state: 1,
-    tokenCount: 3,
-    slotsCount: 3,
-    health: 12,
-  },
-  {
-    id: 91,
-    rarity: 'legendary',
-    state: 0,
-    tokenCount: 20,
-    slotsCount: 5,
-    health: 98,
-  },
-  {
-    id: 911,
-    rarity: 'epic',
-    state: 0,
-    tokenCount: 10,
-    slotsCount: 4,
-    health: 43,
-  }
-];
+import axiosInstance from '../../axios.js';
 
  ActivatedSelector.propTypes = {
   active: PropTypes.bool.isRequired,
@@ -218,10 +104,10 @@ function ItemAi({ ai, onInfoClick, onConnectClick, onSellClick }) {
     <div className={s.item}>
       <div className={s.itemIcon}>
         <div className={`${s.itemImage} ${s.border}`}>
-          <img className={ai.state ? `glow-${ai.rarity}` : ''} src={getAiImage(ai.rarity)}></img>
+          <img className={ai.state ? `glow-${getRarity(ai.rarity)}` : ''} src={getAiImage(getRarity(ai.rarity))}></img>
         </div>
-        <div className={`${s.itemRarity} ${s.border} text-${ai.rarity}`}>
-          {ai.rarity}
+        <div className={`${s.itemRarity} ${s.border} text-${getRarity(ai.rarity)}`}>
+          {getRarity(ai.rarity)}
         </div>
       </div>
       <div className={s.itemMenu}>
@@ -291,10 +177,10 @@ function ItemPc({ pc, onInfoClick, onSellClick }) {
     <div className={s.item}>
       <div className={s.itemIcon}>
         <div className={`${s.itemImage} ${s.border}`}>
-          <img className={pc.state ? `glow-${pc.rarity}` : ''} src={getPcImage(pc.rarity)}></img>
+          <img className={pc.state ? `glow-${getRarity(pc.rarity)}` : ''} src={getPcImage(getRarity(pc.rarity))}></img>
         </div>
-        <div className={`${s.itemRarity} ${s.border} text-${pc.rarity}`}>
-          {pc.rarity}
+        <div className={`${s.itemRarity} ${s.border} text-${getRarity(pc.rarity)}`}>
+          {getRarity(pc.rarity)}
         </div>
       </div>
       <div className={s.itemMenu}>
@@ -361,7 +247,7 @@ function ListAi({ ai, setAi, isActive, setIsActive }) {
         setNotification(false);
         setAi({});
       }}>
-        <img src={getAiImage(ai.rarity)} className={`glow-${ai.rarity}`}></img>
+        <img src={getAiImage(getRarity(ai.rarity))} className={`glow-${getRarity(ai.rarity)}`}></img>
         <span>
           Listed
           <PiCheckBold></PiCheckBold>
@@ -374,8 +260,8 @@ function ListAi({ ai, setAi, isActive, setIsActive }) {
       }}>
         <div className={listStyle.container}>
           <div className={listStyle.image}>
-            <img src={getAiImage(ai.rarity)} className={`glow-${ai.rarity}`}></img>
-            <span className={`text-${ai.rarity}`}>{ai.rarity}</span>
+            <img src={getAiImage(getRarity(ai.rarity))} className={`glow-${getRarity(ai.rarity)}`}></img>
+            <span className={`text-${getRarity(ai.rarity)}`}>{getRarity(ai.rarity)}</span>
           </div>
           <div className={listStyle.controlsContainer}>
             <Input value={price} setValue={setPrice} placeholder='Price'></Input>
@@ -415,7 +301,7 @@ function ListPc({ pc, setPc, isActive, setIsActive }) {
         setNotification(false);
         setPc({});
       }}>
-        <img src={getPcImage(pc.rarity)} className={`glow-${pc.rarity}`}></img>
+        <img src={getPcImage(getRarity(pc.rarity))} className={`glow-${getRarity(pc.rarity)}`}></img>
         <span>
           Listed
           <PiCheckBold></PiCheckBold>
@@ -428,8 +314,8 @@ function ListPc({ pc, setPc, isActive, setIsActive }) {
       }}>
         <div className={listStyle.container}>
           <div className={listStyle.image}>
-            <img src={getPcImage(pc.rarity)} className={`glow-${pc.rarity}`}></img>
-            <span className={`text-${pc.rarity}`}>{pc.rarity}</span>
+            <img src={getPcImage(getRarity(pc.rarity))} className={`glow-${getRarity(pc.rarity)}`}></img>
+            <span className={`text-${getRarity(pc.rarity)}`}>{getRarity(pc.rarity)}</span>
           </div>
           <div className={listStyle.controlsContainer}>
             <Input value={price} setValue={setPrice} placeholder='Price'></Input>
@@ -453,6 +339,9 @@ function ListPc({ pc, setPc, isActive, setIsActive }) {
 }
 
 export default function Inventory() {
+  const [aiData, setAiData] = useState(null);
+  const [pcData, setPcData] = useState(null);
+
   const [active, setActive] = useState(false);
   const [notActive, setNotActive] = useState(false);
 
@@ -484,56 +373,30 @@ export default function Inventory() {
     setPcInfo({});
   }
 
+  useEffect(() => {
+    axiosInstance.get('/api/inventory/pc')
+      .then((res) => {
+        setPcData(res.data);
+      })
+
+    axiosInstance.get('/api/inventory/ai')
+      .then((res) => {
+        setAiData(res.data);
+      })
+  }, [])
+
+  if (!pcData + !aiData) {
+    return (
+      <LoadingScreen></LoadingScreen>
+    )
+  }
+
   return (
     <>
       <ConnectPc isActive={connectAi} setIsActive={setConnectAi} curAi={aiConnectionData} setCurAi={setAiConnectionData}></ConnectPc>
       <PopUp isActive={showAiInfo} onClose={closeAiInfo}>
         <div className={infoPopUpStyle.container}>
-          <div className={infoPopUpStyle.image}>
-            <img src={getAiImage(aiInfo.rarity)} className={`glow-${aiInfo.rarity}`}></img>
-          </div>
-          <div className={infoPopUpStyle.centeredText}>
-            Rarity: <span className={`text-${aiInfo.rarity}`} style={{ fontWeight: 'bold', fontSize: 'large' }}>{aiInfo.rarity}</span>
-          </div>
-          <div className={infoPopUpStyle.centeredText}>
-            This <span className='greenHighlight'>AI</span> has not been used yet. <span className='greenHighlight'>Connect PC</span> to it to start using. Then the AI will start to bring you profit
-          </div>
-          Every PC connected to this AI will be farming tokens during this time:<br />
-          <span className={`${infoPopUpStyle.iconWrapper} greenHighlight`} style={{ fontSize: 'x-large' }}>
-            <PiClockBold></PiClockBold>
-            {aiInfo.timer} Hours
-          </span>
-          <br />
-          You can link this many PCs to the AI:
-          <span className={`${infoPopUpStyle.iconWrapper} text-common`}>
-            <PiDesktopBold></PiDesktopBold>
-            N Common PCs
-          </span>
-          <span className={`${infoPopUpStyle.iconWrapper} text-uncommon`}>
-            <PiDesktopBold></PiDesktopBold>
-            N Uncommon PCs
-          </span>
-          <span className={`${infoPopUpStyle.iconWrapper} text-rare`}>
-            <PiDesktopBold></PiDesktopBold>
-            N Rare PCs
-          </span>
-          <span className={`${infoPopUpStyle.iconWrapper} text-epic`}>
-            <PiDesktopBold></PiDesktopBold>
-            N Epic PCs
-          </span>
-          <span className={`${infoPopUpStyle.iconWrapper} text-legendary`}>
-            <PiDesktopBold></PiDesktopBold>
-            N Legendary PCs
-          </span>
-          <span className={`${infoPopUpStyle.iconWrapper} text-ultra`}>
-            <PiDesktopBold></PiDesktopBold>
-            N Ultra PCs
-          </span>
-          <span className={`${infoPopUpStyle.iconWrapper} text-mythic`}>
-            <PiDesktopBold></PiDesktopBold>
-            N Mythic PCs
-          </span>
-
+          <AiGeneralInfo aiInfo={aiInfo}></AiGeneralInfo>
         </div>
         <div className={infoPopUpStyle.buttonContainer}>
           <GlowingButton onClick={() => {
@@ -547,57 +410,7 @@ export default function Inventory() {
 
       <PopUp isActive={showPcInfo} onClose={closePcInfo}>
         <div className={infoPopUpStyle.container}>
-          <div className={infoPopUpStyle.image}>
-            <img src={getPcImage(pcInfo.rarity)} className={`glow-${pcInfo.rarity}`}></img>
-          </div>
-          <div className={infoPopUpStyle.centeredText}>
-            Rarity: <span className={`text-${pcInfo.rarity}`} style={{ fontWeight: 'bold', fontSize: 'large' }}>{pcInfo.rarity}</span>
-          </div>
-          <div className={infoPopUpStyle.centeredText}>
-            While the <span className='greenHighlight'>PC is not activated</span>, you can sell it. As soon as you start using it, you will not be able to list it on the market
-          </div>
-          Earnings per hour:<br />
-          <span className={`priceWrapper greenHighlight`} style={{ fontSize: 'x-large' }}>
-            <img src={tokenIcon} style={{ marginRight: '0.2em', marginLeft: '0em' }}></img>
-            {pcInfo.tokenCount}/hour
-          </span>
-          <br />
-          PC duration:<br />
-          <span className={`${infoPopUpStyle.iconWrapper} greenHighlight`}>
-            <PiClockBold></PiClockBold>
-            N Hours
-          </span>
-          <br />
-          This PC takes up this many slots for each AI:
-          <span className={`${infoPopUpStyle.aiSlotsWrapper} text-common`}>
-            <img src={getAiImage('common')} className='glow-common'></img>
-            N for common AI
-          </span>
-          <span className={`${infoPopUpStyle.aiSlotsWrapper} text-uncommon`}>
-            <img src={getAiImage('uncommon')} className='glow-uncommon'></img>
-            N for uncommon AI
-          </span>
-          <span className={`${infoPopUpStyle.aiSlotsWrapper} text-rare`}>
-            <img src={getAiImage('rare')} className='glow-rare'></img>
-            N for rare AI
-          </span>
-          <span className={`${infoPopUpStyle.aiSlotsWrapper} text-epic`}>
-            <img src={getAiImage('epic')} className='glow-epic'></img>
-            N for epic AI
-          </span>
-          <span className={`${infoPopUpStyle.aiSlotsWrapper} text-legendary`}>
-            <img src={getAiImage('legendary')} className='glow-legendary'></img>
-            N for legendary AI
-          </span>
-          <span className={`${infoPopUpStyle.aiSlotsWrapper} text-ultra`}>
-            <img src={getAiImage('ultra')} className='glow-ultra'></img>
-            N for ultra AI
-          </span>
-          <span className={`${infoPopUpStyle.aiSlotsWrapper} text-mythic`}>
-            <img src={getAiImage('mythic')} className='glow-mythic'></img>
-            N for mythic AI
-          </span>
-
+          <PcGeneralInfo pcInfo={pcInfo}></PcGeneralInfo>
         </div>
       </PopUp>
 
@@ -611,10 +424,10 @@ export default function Inventory() {
         {
           showAi || showAi == showPc ?
             aiData.map((ai) => {
-              if (active && !ai.state) {
+              if (active && !ai.is_activated) {
                 return <></>
               }
-              if (notActive && ai.state) {
+              if (notActive && ai.is_activated) {
                 return <></>
               }
               return <ItemAi key={ai.id} ai={ai}
@@ -636,10 +449,10 @@ export default function Inventory() {
         {
           showPc || showAi == showPc ?
             pcData.map((pc) => {
-              if (active && !pc.state) {
+              if (active && !pc.is_activated) {
                 return <></>
               }
-              if (notActive && pc.state) {
+              if (notActive && pc.is_activated) {
                 return <></>
               }
               return <ItemPc key={pc.id} pc={pc}
